2b9cd6098c08ca620314879deaac0b69ce7cd998
mattchupp <mattlchupp@gmail.com>
2019-03-06 14:40:12 -0600

    added registration info and when,time,where or conf

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 import "../components/uikit.min.css"
 import "../components/layout.css"
+import "../images/s+d.png"
 import Speaker from "../components/speaker"
 
 import SEO from "../components/seo"
 
 const videoURL= 'https://player.vimeo.com/video/318806849';
+const registerURL= 'https://thevineyardchurch.ccbchurch.com/goto/forms/915/responses/new';
 
 const Putty = {
   name: "Putty Putman",
@@ -70,10 +72,6 @@ const speakerTitle = {
   fontWeight: '800'
 }
 
-const promoVideo = {
-
-}
-
 const About = () => (
   <div style={theme}>
     <SEO title="About" />
@@ -87,7 +85,40 @@ const About = () => (
           </iframe>
         </div>
         <div className="uk-width-expand@m">
-          <p>About</p>
+          <p>We have been restored to perfect unity with our Heavenly Father.
+            Our relationship with Him is one that does not include our past
+            mistakes but unlocks endless possibilities within our present and
+            our future. We are the children of God. Made in His Image to reflect
+            Him in the world around us. Because of Jesus' sacrifice, we are free
+            to become all that we were created to be. Our destiny is to change
+            environments and release the Kingdom of Heaven on this earth.
+            We are fully empowered by the very Spirit of God.
+            We are the sons and daughters of Heaven.
+          </p>
+          <p>
+          Sons & Daughters is a conference dedicated to training, equipping,
+          and encouraging students to step into the fullness of their identity
+          as sons & daughters of God. Join us June 20th-22nd as we journey
+          together through teaching, activation, and practice in supernatural
+          ministry. We will cover topics such as prophecy, healing, words of
+          knowledge, deliverance, and power evangelism.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <p>
+          <span className="uk-text-bold">When:</span> June 20th - 22nd <br />
+          <span className="uk-text-bold">Time:</span> Friday: 6pm- 10pm  // Saturday: 8am-10pm <br />
+          <span className="uk-text-bold">Where:</span> The Vineyard Church of Central Illinois, 1500 N. Lincoln Ave. Urbana, IL 61801
+        </p>
+        <p>
+        <span className="uk-text-bold">Cost: $89</span> ($99 after Early-Bird ENDS on March 31st) <br />
+        Week of: $109 June 17th-June 20th <br />
+        (No refunds after June 16th)
+        </p>
+        <div className="uk-text-center">
+          <a className="uk-button uk-button-secondary uk-margin-right uk-button-large" href={registerURL}>Register</a>
         </div>
       </div>
 
