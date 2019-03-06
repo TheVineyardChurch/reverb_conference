7b97461d59d65d62377f30368f9b9a5c564cec2e
mattchupp <mattlchupp@gmail.com>
2019-03-06 10:06:05 -0600

    code cleanup

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,41 +10,28 @@ const registerURL= 'https://thevineyardchurch.ccbchurch.com/goto/forms/915/respo
 
 const Landing = () => (
 
-
-      // <h1 style={{ margin: 0 }}>
-      //   <Link
-      //     to="/"
-      //     style={{
-      //       color: `white`,
-      //       textDecoration: `none`,
-      //     }}
-      //   >
-      //   </Link>
-      // </h1>
-
-      <div className="vimeo-wrapper">
-        <iframe
-          src={videoURL} title="Sons and Daughters Promo"
-          frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
-        </iframe>
-        <div
-            style={{
-              position: 'absolute',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh',
-              width: '100%',
-              pointerEvents: 'all'
-            }}
-          >
-          <div className="landing-links">
-            <a className="uk-button uk-button-secondary uk-margin-right uk-button-large" href={registerURL}>Early-Bird</a>
-            <Link className="uk-button uk-button-secondary uk-button-large" to='/about'>Learn More</Link>
-          </div>
-        </div>
+  <div className="vimeo-wrapper">
+    <iframe
+      src={videoURL} title="Sons and Daughters Promo"
+      frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+    </iframe>
+    <div
+        style={{
+          position: 'absolute',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          width: '100%',
+          pointerEvents: 'all'
+        }}
+      >
+      <div className="landing-links">
+        <a className="uk-button uk-button-secondary uk-margin-right uk-button-large" href={registerURL}>Early-Bird</a>
+        <Link className="uk-button uk-button-secondary uk-button-large" to='/about'>Learn More</Link>
       </div>
-
+    </div>
+  </div>
 
 )
 
