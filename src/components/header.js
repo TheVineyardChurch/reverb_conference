588606eb60624b4f6a41273d599a7052f9733d5f
mattchupp <mattlchupp@gmail.com>
2019-03-06 15:19:35 -0600

    moved header info to component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,47 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const videoURL= 'https://player.vimeo.com/video/318806849';
+
+const confTitle = {
+  color: '#222222',
+  textTransform: 'uppercase',
+  fontWeight: '800'
+}
+
+const Header = () => (
+  <div>
+    <h1 className="uk-heading-hero" style={confTitle}>Sons & Daughters</h1>
+    <div className="uk-grid uk-margin-large-bottom">
+      <div className="uk-width-auto@m">
+        <iframe className="promo-video"
+          src={videoURL} title="Sons and Daughters Promo"
+          frameborder="0" width="640" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+        </iframe>
+      </div>
+      <div className="uk-width-expand@m">
+        <p>We have been restored to perfect unity with our Heavenly Father.
+          Our relationship with Him is one that does not include our past
+          mistakes but unlocks endless possibilities within our present and
+          our future. We are the children of God. Made in His Image to reflect
+          Him in the world around us. Because of Jesus' sacrifice, we are free
+          to become all that we were created to be. Our destiny is to change
+          environments and release the Kingdom of Heaven on this earth.
+          We are fully empowered by the very Spirit of God.
+          We are the sons and daughters of Heaven.
+        </p>
+        <p>
+        Sons & Daughters is a conference dedicated to training, equipping,
+        and encouraging students to step into the fullness of their identity
+        as sons & daughters of God. Join us June 20th-22nd as we journey
+        together through teaching, activation, and practice in supernatural
+        ministry. We will cover topics such as prophecy, healing, words of
+        knowledge, deliverance, and power evangelism.
+        </p>
+      </div>
     </div>
-  </header>
+  </div>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import "../components/uikit.min.css"
 import "../components/layout.css"
-import "../images/s+d.png"
 
 import Guests from "../components/guests"
+import Header from "../components/header"
 
 import SEO from "../components/seo"
 
-const videoURL= 'https://player.vimeo.com/video/318806849';
 const registerURL= 'https://thevineyardchurch.ccbchurch.com/goto/forms/915/responses/new';
 
 const theme = {
@@ -16,49 +15,15 @@ const theme = {
   color: '#222222',
 }
 
-const confTitle = {
-  color: '#222222',
-  textTransform: 'uppercase',
-  fontWeight: '800'
-}
-
-
 /* exported for about page */
 const About = () => (
   <div style={theme}>
     <SEO title="About" />
     <div className="uk-container">
-      <h1 className="uk-heading-hero" style={confTitle}>Sons & Daughters</h1>
-      <div className="uk-grid uk-margin-large-bottom">
-        <div className="uk-width-auto@m">
-          <iframe className="promo-video"
-            src={videoURL} title="Sons and Daughters Promo"
-            frameborder="0" width="640" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen>
-          </iframe>
-        </div>
-        <div className="uk-width-expand@m">
-          <p>We have been restored to perfect unity with our Heavenly Father.
-            Our relationship with Him is one that does not include our past
-            mistakes but unlocks endless possibilities within our present and
-            our future. We are the children of God. Made in His Image to reflect
-            Him in the world around us. Because of Jesus' sacrifice, we are free
-            to become all that we were created to be. Our destiny is to change
-            environments and release the Kingdom of Heaven on this earth.
-            We are fully empowered by the very Spirit of God.
-            We are the sons and daughters of Heaven.
-          </p>
-          <p>
-          Sons & Daughters is a conference dedicated to training, equipping,
-          and encouraging students to step into the fullness of their identity
-          as sons & daughters of God. Join us June 20th-22nd as we journey
-          together through teaching, activation, and practice in supernatural
-          ministry. We will cover topics such as prophecy, healing, words of
-          knowledge, deliverance, and power evangelism.
-          </p>
-        </div>
-      </div>
 
-      <div>
+      <Header />
+
+      <div className="uk-margin-large-bottom">
         <p>
           <span className="uk-text-bold">When:</span> June 20th - 22nd <br />
           <span className="uk-text-bold">Time:</span> Friday: 6pm- 10pm  // Saturday: 8am-10pm <br />
@@ -74,9 +39,8 @@ const About = () => (
         </div>
       </div>
 
-
       <Guests />
-      
+
       <Link to="/">Go back to the homepage</Link>
     </div>
   </div>
