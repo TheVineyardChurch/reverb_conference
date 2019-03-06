1a9d256d0a14f76e05caa92b31b57f7e51cf441e
mattchupp <mattlchupp@gmail.com>
2019-03-06 17:01:36 -0600

    added new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,9 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#222222`,
-        theme_color: `#663399`,
+        theme_color: `#222222`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sd.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
